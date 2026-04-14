9313428ad2a10f5c04cbd9ee426a03ff13441416
JaySGoenka <jaygoenka.15@gmail.com>
2026-04-14 15:53:35 -0500

    Fault disabled for testing

--- bms_2025/Core/Src/main.c
+++ bms_2025/Core/Src/main.c
@@ -212,7 +212,7 @@ static void poll_cell_temps_once(void)
  */
 static void refresh_fault_state(void)
 {
-  bmsFault = FAULT_check(&BMSCriticalInfo, &BMSConfig, BMS_STATUS);
+  // bmsFault = FAULT_check(&BMSCriticalInfo, &BMSConfig, BMS_STATUS); temporarily disable fault checking for bench testing
 
   if (bmsFault) {
       HAL_GPIO_WritePin(BMS_FLT_EN_GPIO_Port, BMS_FLT_EN_Pin, GPIO_PIN_SET);
